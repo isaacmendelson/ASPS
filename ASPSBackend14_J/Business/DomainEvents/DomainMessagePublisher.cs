206628808d083720bef4ea1bc6ead13e2a385904
isaacm <isaacmendelson@gmail.com>
2026-04-16 21:27:18 +0300

    refactored IBAckgroundTask, IDomainEvent

--- ASPSBackend14_J/Business/DomainEvents/DomainMessagePublisher.cs
+++ ASPSBackend14_J/Business/DomainEvents/DomainMessagePublisher.cs
@@ -1,4 +1,5 @@
 ﻿using Common.Infrastructure.Tasks;
+using Common.Interfaces;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
---
+++ ASPSBackend14_J/Common/Interfaces/IBackgroundTask.cs
@@ -0,0 +1,7 @@
+namespace Common.Interfaces;
+
+public interface IBackgroundTask
+{
+    void Start();
+    void Stop();
+}
